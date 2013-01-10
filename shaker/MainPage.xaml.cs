3c6fb01a27b0525fef91cc002ce355c15f4abbf5
Anthony Vong <chibiano@yahoo.com>
2013-01-10 15:37:36 -0800

    working accelerometer

---
+++ shaker/MainPage.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+using Microsoft.Devices.Sensors;
+using Microsoft.Xna.Framework;
+
+namespace shaker
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+
+        //Accelerometer accelerometer;
+       // private Vector3 _lastreading;
+        private ShakeDetect _shakeDetect;
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            _shakeDetect = new ShakeDetect();
+            _shakeDetect.ShakeEvent += new EventHandler<EventArgs>(_shakeDetect_ShakeEvent);
+            _shakeDetect.Start();
+/*
+            if (!Accelerometer.IsSupported)
+            {
+                statusTextBlock.Text = "device does not support accelerometer";
+                startButton.IsEnabled = false;
+                stopButton.IsEnabled = false;
+            }
+*/
+        }
+
+        void _shakeDetect_ShakeEvent(object sender, EventArgs e)
+        {
+            this.Dispatcher.BeginInvoke(() =>
+                {
+                    Storyboard shakeAnimation = Resources["ShakeAnimation"] as Storyboard;
+                    shakeAnimation.Begin();
+                });
+        }
+
+        /*
+        private void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (accelerometer == null)
+            {
+                accelerometer = new Accelerometer();
+                accelerometer.TimeBetweenUpdates = TimeSpan.FromMilliseconds(60);
+                accelerometer.CurrentValueChanged += new EventHandler<SensorReadingEventArgs<AccelerometerReading>>(accelerometer_CurrentValueChanged);
+            }
+
+            try
+            {
+                statusTextBlock.Text = "starting accelerometer";
+                accelerometer.Start();
+            }
+            catch (InvalidOperationException ex)
+            {
+                statusTextBlock.Text = "unable to start accelerometer";
+            }
+        }
+
+        void accelerometer_CurrentValueChanged(object sender, SensorReadingEventArgs<AccelerometerReading> e)
+        {
+            Dispatcher.BeginInvoke(() => UpdateUI(e.SensorReading));
+        }
+
+
+
+        private void UpdateUI(AccelerometerReading accelerometerReading)
+        {
+            statusTextBlock.Text = "getting data";
+
+            Vector3 acceleration = accelerometerReading.Acceleration;
+
+            if (_lastreading != null)
+            {
+                if (shaked(_lastreading, acceleration))
+                {
+                    try
+                    {
+                        statusTextBlock.Text = "shake shake shake";
+                    }
+                    catch (InvalidOperationException ex)
+                    {
+                        statusTextBlock.Text = "unable to shake";
+                    }
+                }
+            }
+            _lastreading = acceleration;
+
+            //numeric values
+            xTextBlock.Text = "X: " + acceleration.X.ToString("0.00");
+            yTextBlock.Text = "Y: " + acceleration.Y.ToString("0.00");
+            zTextBlock.Text = "Z: " + acceleration.Z.ToString("0.00");
+
+            //graphical values
+            xLine.X2 = xLine.X1 + acceleration.X * 200;
+            yLine.Y2 = yLine.Y1 - acceleration.Y * 200;
+            zLine.X2 = zLine.X1 - acceleration.Z * 100;
+            zLine.Y2 = zLine.Y1 + acceleration.Z * 100;
+        }
+
+        private bool shaked(Vector3 _lastreading, Vector3 acceleration)
+        {
+            double deltaY = Math.Abs(acceleration.Y) + Math.Abs(_lastreading.Y);
+            if (deltaY > 1.8)
+                return true;
+            else
+                return false;
+        }
+
+        private void stopButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (accelerometer != null)
+            {
+                accelerometer.Stop();
+                statusTextBlock.Text = "acceleromater stopped";
+            }
+        }
+          */
+    }
+}
---
+++ shaker/ShakeDetect.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Devices.Sensors;
+
+namespace shaker
+{
+    public class ShakeDetect
+    {
+        private Accelerometer _accelerometer = null;
+        object SyncRoot = new object();
+        private int _minimumShake;
+        ShakeRecord[] _shakeRecordList;
+        private int _shakeRecordIndex = 0;
+        private const double MinAccelMagnitude = 1.2;
+        private const double MinAccelMagnitudeSquared = MinAccelMagnitude * MinAccelMagnitude;
+        private static readonly TimeSpan MinShakeTime = TimeSpan.FromMilliseconds(500);
+
+        public event EventHandler<EventArgs> ShakeEvent = null;
+
+        protected void OnShakeEvent()
+        {
+            if (ShakeEvent != null)
+                ShakeEvent(this, new EventArgs());
+        }
+
+
+        [Flags]
+        public enum Direction
+        {
+            None = 0,
+            North = 1,
+            South = 2,
+            East = 8,
+            West = 4,
+            NorthWest = North | West,
+            SouthWest = South | West,
+            SouthEast = South | East,
+            NorthEast = North | East
+        };
+
+        public struct ShakeRecord
+        {
+            public Direction ShakeDirection;
+            public DateTime EventTime;
+        }
+
+        public ShakeDetect()
+            : this(2)
+        { 
+        }
+
+        public ShakeDetect(int minShakes)
+        {
+            _minimumShake = minShakes;
+            _shakeRecordList = new ShakeRecord[minShakes];
+        }
+
+        public void Start()
+        {
+            lock (SyncRoot)
+            {
+                if (_accelerometer == null)
+                {
+                    _accelerometer = new Accelerometer();
+                    _accelerometer.ReadingChanged += new EventHandler<AccelerometerReadingEventArgs>(_accelerometer_ReadingChanged);
+                    _accelerometer.Start();
+                }
+            }
+        }
+
+        public void Stop()
+        {
+            lock (SyncRoot)
+            {
+                if (_accelerometer != null)
+                {
+                    _accelerometer.Stop();
+                    _accelerometer.ReadingChanged -= _accelerometer_ReadingChanged;
+                    _accelerometer = null;
+                }
+            }
+        }
+
+        Direction DegreesToDirection(double direction)
+        {
+            if ((direction >= 337.5) || (direction <= 22.5))
+                return Direction.North;
+            if ((direction <= 67.5))
+                return Direction.NorthEast;
+            if (direction <= 112.5)
+                return Direction.East;
+            if (direction <= 157.5)
+                return Direction.SouthEast;
+            if (direction <= 202.5)
+                return Direction.South;
+            if (direction <= 247.5)
+                return Direction.SouthWest;
+            if (direction <= 292.5)
+                return Direction.West;
+            return Direction.NorthWest;
+        }
+
+        void CheckForShakes()
+        {
+            int startIndex = (_shakeRecordIndex - 1);
+            if (startIndex < 0)
+                startIndex = _minimumShake - 1;
+            int endIndex = _shakeRecordIndex;
+
+            if ((_shakeRecordList[endIndex].EventTime.Subtract(_shakeRecordList[startIndex].EventTime)) <= MinShakeTime)
+            {
+                OnShakeEvent();
+            }
+        }
+
+        void _accelerometer_ReadingChanged(object sender, AccelerometerReadingEventArgs e)
+        {
+            if ((e.X * e.X + e.Y * e.Y) > MinAccelMagnitudeSquared)
+            {
+                double degrees = 180.0 * Math.Atan2(e.Y, e.X) / Math.PI;
+                Direction direction = DegreesToDirection(degrees);
+
+                if ((direction & _shakeRecordList[_shakeRecordIndex].ShakeDirection) != Direction.None)
+                    return;
+                ShakeRecord record = new ShakeRecord();
+                record.EventTime = DateTime.Now;
+                record.ShakeDirection = direction;
+                _shakeRecordIndex = (_shakeRecordIndex + 1) % _minimumShake;
+                _shakeRecordList[_shakeRecordIndex] = record;
+
+                CheckForShakes();
+            }
+        }
+
+    }
+}
